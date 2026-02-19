2e676845622d27d24f9ac54f4f00254e030f18b2
sagarboyal <sboyal12@gmail.com>
2026-02-19 11:47:18 +0530

    setup axios and add it in homescreen

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,8 +1,12 @@
 import ProviderCard from "@/src/components/providers/card/ProviderCard";
+import api from "@/src/services/api";
+import { COLORS } from "@/src/theme/colors";
 import { IMAGES } from "@/src/theme/images";
+import { showError } from "@/src/utils/toast";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
+  ActivityIndicator,
   FlatList,
   ImageBackground,
   Pressable,
@@ -13,18 +17,35 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "./HomeScreen.styles";
-import { Providers } from "./data";
 
 const HomeScreen = () => {
-  const [providerList, setProviderList] = useState(Providers);
+  const [loading, setLoading] = useState(false);
+  const [providerList, setProviderList] = useState([]);
 
   const renderProviders = ({ item }: any) => {
     return <ProviderCard data={item} />;
   };
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      setLoading(true);
+      try {
+        const response = await api.get("/v1/provider");
+        setProviderList(response.data);
+      } catch (error) {
+        console.log("error:", error);
+        showError("Something went wrong.");
+      } finally {
+        setLoading(true);
+      }
+    };
+
+    fetchProviders();
+  }, []);
   return (
     <SafeAreaView style={styles.screen}>
       <StatusBar barStyle={"dark-content"} />
-
+      {loading && <ActivityIndicator size="large" color={COLORS.primary} />}
       <View style={styles.header}>
         <ImageBackground
           source={IMAGES.waves}
@@ -54,7 +75,7 @@ const HomeScreen = () => {
 
         <FlatList
           data={providerList}
-          keyExtractor={(item) => item.id.toString()}
+          keyExtractor={(item) => item.id}
           renderItem={renderProviders}
           horizontal
           showsHorizontalScrollIndicator={false}
